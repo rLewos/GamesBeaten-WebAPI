8e156ef028f3fd56c481952bf709d684b7669756
rLewos <21082650+rLewos@users.noreply.github.com>
2023-02-03 14:25:40 -0300

    Entities: - Game - Developer - Plataform - Publisher - Developer
    Domain Tests.
    - Game

---
+++ GamesBeaten/Entities/Developer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesBeaten.Domain.Entities
+{
+    public class Developer : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public Developer(string name)
+        {
+            Name= name;
+        }
+    }
+}
---
+++ GamesBeaten/Entities/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesBeaten.Domain.Entities
+{
+    public class Game : BaseEntity
+    {
+        public string Title { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public IList<Plataform>? PlataformList { get; set; }
+
+        public Game()
+        {
+            
+        }
+
+        public Game(string title)
+        {
+            Title = title;
+        }
+
+        public void AddPlataform(Plataform plataform)
+        {
+            if (plataform == null)
+                throw new ArgumentNullException("Plataform is empty.");
+
+            if (PlataformList == null)
+                PlataformList = new List<Plataform>();
+
+            PlataformList.Add(plataform);
+        }
+    }
+}
